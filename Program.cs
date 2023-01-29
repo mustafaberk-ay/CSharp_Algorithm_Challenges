2d0b5d132f9be071ff21ceee745c8645cb66ca8f
Mustafa Berk Ay <70906114+mustafaberk01@users.noreply.github.com>
2023-01-29 20:46:26 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+static Dictionary<int, int> CountEachElement(int[] arr)
+{
+    Dictionary<int, int> result = new Dictionary<int, int>();
+	foreach (var item in arr)
+	{
+		if (result.ContainsKey(item))
+		{
+			result[item]++;
+		}
+		else
+		{
+			result.Add(item, 1);
+		}
+	}
+	return result;
+}
+
+foreach (var kvp in CountEachElement(new int[] {1,5,6,8,2,1,6,5}))
+{
+	Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+}
